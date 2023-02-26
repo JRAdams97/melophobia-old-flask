be0d7b0e0575284c2e317f7080dc4fb059b6536a
justinadams7991@gmail.com <justinadams7991@gmail.com>
2023-02-26 23:37:46 +1100

    feat(api): Implement release detail api
    - add css rules for card titles and subtitles to improve readability
    - add custom sorting for `releases_list.html` to sort by year,
      month, day
    - add `release_detail.html` template to display release details
    - add `releases/<_id>` route to provide details of a specific
      release
    - add two new test releases to verify release list sorting
    - fix artist fallback image rendering
    - remove `hull_rating` field from db and ddl

--- melophobia/releases/views.py
+++ melophobia/releases/views.py
@@ -1,3 +1,4 @@
+from bson import ObjectId
 from flask import Blueprint, render_template
 
 from melophobia.db import get_db
@@ -30,9 +31,44 @@ def list_releases():
                 ],
                 'as': "genres"
             }
-        }, {
-            '$sort': {"title": 1}
         }
     ]))
 
+    releases = sorted(releases, key=lambda rd: (rd['release_date'].split('/')[2],
+                                                rd['release_date'].split('/')[1],
+                                                rd['release_date'].split('/')[0]), reverse=True)
+
     return render_template('releases_list.html', releases=releases)
+
+
+@releases_bp.route('/releases/<_id>')
+def release_detail(_id):
+    mongo = get_db()
+
+    release = list(mongo.db.releases.aggregate([
+        {
+            '$match': {'_id': ObjectId(_id)}
+        }, {
+            '$lookup': {
+                'from': "artists",
+                'let': {'artists': "$artists"},
+                'pipeline': [
+                    {'$match': {'$expr': {'$in': ["$_id", "$$artists"]}}},
+                    {'$sort': {"name": 1}}
+                ],
+                'as': "artists"
+            }
+        }, {
+            '$lookup': {
+                'from': "genres",
+                'let': {'genres': "$genres"},
+                'pipeline': [
+                    {'$match': {'$expr': {'$in': ["$_id", "$$genres"]}}},
+                    {'$sort': {"name": 1}}
+                ],
+                'as': "genres"
+            }
+        }
+    ]))
+
+    return render_template('release_detail.html', release=release[0])
